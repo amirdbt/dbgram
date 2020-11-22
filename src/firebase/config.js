e2a0482a1e9ae930ffb506a3f8c31d0e3070cee1
Ahmed Dambatta Hassan <hassanamir643@yahoo.com>
2020-11-22 14:02:21 +0100

    added firebase package to application

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCSZwgPxXWDipHl1w_zoHzsCR4RsebHlBk",
+  authDomain: "dbgram-4c7e5.firebaseapp.com",
+  databaseURL: "https://dbgram-4c7e5.firebaseio.com",
+  projectId: "dbgram-4c7e5",
+  storageBucket: "dbgram-4c7e5.appspot.com",
+  messagingSenderId: "274740005088",
+  appId: "1:274740005088:web:37b9633b3ba83f07bfa885",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
